a7289f7ce4ddc7091b37d1afd580395ef6842e54
qiaoshouzi <61568944+qiaoshouzi@users.noreply.github.com>
2022-06-07 06:14:52 +0800

    chore (package.json): Update
    + Add `vuex`
    + Update `vue-recaptcha` 2.0.1 -> 2.0.2
    - Delete `@vicons/ionicons4` `vfonts`

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import { createApp } from "vue";
 import App from "@/App.vue";
 import router from "@/router";
-// import axios from "axios";
-// import VueAxios from "vue-axios";
-import "vfonts/Lato.css";
+import store from "./store";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(store).use(router).mount("#app");
